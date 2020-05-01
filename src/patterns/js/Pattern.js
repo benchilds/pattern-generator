7fef2164bcb9d55ba5b9b6fc27d4d4f144619057
Ben <ben@benchilds.uk>
2020-05-01 09:34:40 +0100

    Creating a grid generator

--- src/patterns/js/Pattern.js
+++ src/patterns/js/Pattern.js
@@ -4,9 +4,52 @@ export function Pattern() {
 
   console.log(`Pattern`);
 
+  const colNum = 10;
+  const cols = [];
+
+  const rowNum = 5;
+  const rows = [];
+
+  const items = [];
+
+  for (let c = 1; c <= colNum; c++) {
+    cols.push(React.createElement(
+      'p',
+      { key: 'col-' + c },
+      c
+    ));
+  }
+
+  for (let r = 1; r <= rowNum; r++) {
+    rows.push(React.createElement(
+      'p',
+      { key: 'row-' + r },
+      r
+    ));
+  }
+
+  for (let i = 1; i <= colNum * rowNum; i++) {
+
+    let currentRow = i % rowNum === 0 ? i : i % rowNum;
+    let currentCol = i % colNum === 0 && i >= colNum ? i : i % colNum;
+
+    let x = i <= colNum ? i : 10;
+    let y = i <= rowNum ? i : 10;
+    items.push(React.createElement(
+      'div',
+      { key: i },
+      i + ' x-' + x + ' y-' + y + ' row-' + currentRow + ' col-' + currentCol
+    ));
+    x++;
+  }
+
   return React.createElement(
-    "div",
+    'div',
     null,
-    "Pattern"
+    'Pattern',
+    React.createElement('br', null),
+    items,
+    cols,
+    rows
   );
 }
--- src/patterns/jsx/Pattern.jsx
+++ src/patterns/jsx/Pattern.jsx
@@ -4,9 +4,40 @@ export function Pattern() {
 
   console.log(`Pattern`);
 
+  const colNum = 10;
+  const cols = []
+
+  const rowNum = 5;
+  const rows = []
+
+  const items = []
+
+  for (let c = 1; c <= colNum; c++) {
+    cols.push(<p key={'col-' + c}>{c}</p>)
+  }
+
+  for (let r = 1; r <= rowNum; r++) {
+    rows.push(<p key={'row-' + r}>{r}</p>)
+  }
+
+  for (let i = 1; i <= colNum * rowNum; i++) {
+    
+    let currentRow = i % rowNum === 0 ? i : i % rowNum;
+    let currentCol = i % colNum === 0 && i >= colNum ? i : i % colNum;
+
+    let x = i <= colNum ? i : 10;
+    let y = i <= rowNum ? i : 10;
+    items.push(<div key={i}>{i + ' x-' + x + ' y-' + y  + ' row-' + currentRow + ' col-' + currentCol}</div>)
+    x++;
+  
+  }
+
   return (
     <div>
-      Pattern
+      Pattern<br/>
+      {items}
+      {cols}
+      {rows}
     </div>
   );
 
