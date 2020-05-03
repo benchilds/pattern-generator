2c938cffc01270e7ac0eace11ad75e9bf213281e
Ben <ben@benchilds.uk>
2020-05-03 22:16:35 +0100

    Grid generator calculating properly

--- src/patterns/jsx/Pattern.jsx
+++ src/patterns/jsx/Pattern.jsx
@@ -5,30 +5,16 @@ export function Pattern() {
   console.log(`Pattern`);
 
   const colNum = 10;
-  const cols = []
-
   const rowNum = 5;
-  const rows = []
-
   const items = []
-
-  for (let c = 1; c <= colNum; c++) {
-    cols.push(<p key={'col-' + c}>{c}</p>)
-  }
-
-  for (let r = 1; r <= rowNum; r++) {
-    rows.push(<p key={'row-' + r}>{r}</p>)
-  }
-
+  let x = 0;
+  
   for (let i = 1; i <= colNum * rowNum; i++) {
     
-    let currentRow = i % rowNum === 0 ? i : i % rowNum;
-    let currentCol = i % colNum === 0 && i >= colNum ? i : i % colNum;
+    x = (i - 1) % colNum === 0 ? x + 1 : x;
+    let y = i % colNum === 0 ? colNum : i % colNum;
 
-    let x = i <= colNum ? i : 10;
-    let y = i <= rowNum ? i : 10;
-    items.push(<div key={i}>{i + ' x-' + x + ' y-' + y  + ' row-' + currentRow + ' col-' + currentCol}</div>)
-    x++;
+    items.push(<div key={i}>{i + ' x-' + x + ' y-' + y}</div>)
   
   }
 
@@ -36,8 +22,6 @@ export function Pattern() {
     <div>
       Pattern<br/>
       {items}
-      {cols}
-      {rows}
     </div>
   );
 
